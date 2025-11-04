0d59fc5baa43ddfaa8c7c54ca45eda2b82867123
kuoluo <2802994001@qq.com>
2025-11-04 11:48:15 +0800

    创建main.cpp文件，写了初始代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void sort(int arr[], int size)
+{
+    
+}
+int main()
+{
+    int arr[] = {12, 3, 8, 20, 9, 7, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, size);
+
+    return 0;
+} 
+
